db36e3a3c0a7c951baa0f3e59762139a488a7686
Nkechi-Christabel <udenkwornkechi@gmail.com>
2022-10-11 12:03:22 +0100

    Updated the cors options

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ require("./config/passport");
 
 //Cross-browser-origin
 const corsOptions = {
-  origin: "http://localhost:3000" || process.env.PORT,
+  origin: "http://localhost:3000" || "https://promah-api.herokuapp.com",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 
 //Logging in the console
